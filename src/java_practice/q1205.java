fdfdd80b324250c040fec4cb5eb93dfb88d04161
iamjunsulee <mellow0924@gmail.com>
2018-03-06 18:13:54 +0900

    3. 여전히 런타임 에러..원인 찾는중

--- src/java_practice/q1205.java
+++ src/java_practice/q1205.java
@@ -14,7 +14,8 @@ public class q1205 {
 			scoreList[i] = sc.nextInt();
 		}
 		sc.close();
-		
+		sort_descending(scoreList,n);
+
 		if(scoreList[n-1] >= score && n == p){
 			System.out.println(-1);
 		}else{
@@ -27,6 +28,21 @@ public class q1205 {
 				}
 			}
 			System.out.println(rank);
+		}
+	}
+	
+	public static void sort_descending(int num[], int size){
+		int max,temp;
+		for(int i=0;i<size-1;i++){
+			max = i;
+			for(int j=i;j<size;j++){
+				if(num[j] > num[max]){
+					max = j;
+				}
+			}
+			temp = num[max];
+			num[max] = num[i];
+			num[i] = temp;
 		}
 	}
 }
