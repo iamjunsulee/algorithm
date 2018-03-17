240a1476583d753ee2f7b0b0cf0c880f8b856388
iamjunsulee <mellow0924@gmail.com>
2018-03-17 20:04:08 +0900

    Revert "[BAEKJOON_11004]K번째 수 풀이완료"
    This reverts commit aa0aec4799db27a1f121670868222a3f995f0ef2.

--- src/backjun/sort/q11004.java
+++ src/backjun/sort/q11004.java
@@ -16,59 +16,37 @@ public class q11004 {
         for(int i = 0;i<size;i++){
             data[i] = Integer.parseInt(st.nextToken());
         }
-        dualPivotQuickSort(data, 0, size - 1);
+        quickSort(data,0,size-1);
         bw.write(data[idx-1]+"\n");
         bw.close();
         br.close();
     }
-    public static void dualPivotQuickSort(int[] data,int start,int end){
+
+    public static void quickSort(int[] data, int start, int end){
         if(start < end){
-            //첫번째 index 값을 pivot1, 마지막 index 값을 pivot2로 설정
-            if(data[start] > data[end]){//만약 pivot2가 더 작으면 교환
-                swap(data,start,end);
-            }
-            int pivot1 = data[start];
-            int pivot2 = data[end];
-            int low = start + 1;    //pivot1 다음 index부터 시작
-            int k = start + 1;      //움직이는 index
-            int great = end - 1;    //pivot2 전 index부터 시작
+            int pivot = data[start];	//기준값 설정
+            int left = start+1;			//2 번째 인덱스를 left로 초기 설정
+            int right = end;
+            int temp;
 
-            while(k <= great){
-                if(data[k] < pivot1){   //small 값인 경우
-                    swap(data,k,low);   //low index 값과 교환
-                    low++;              //low 이동
-                }else if(data[k] >= pivot2) {   //large 값인 경우
-                    //large 값인 경우, swap할 값을 찾는다
-                    //pivot2보다 작은 값이 나올때까지 이동
-                    while (data[great] > pivot2) {
-                        great--;
-                    }
-                    if(k > great){  //k, great가 교차되는 경우, pivot1,2 위치 조정
-                        break;
-                    }
-                    swap(data,k,great);
-                    //swap한 후, pivot1보다 작으면 low 값과 교환
-                    if(data[k] < pivot1){
-                        swap(data,k,low);
-                        low++;
-                    }
+            while(left < right){//right가 left보다 크면 정렬이 된 상태
+                while(pivot < data[right] && start < right){right--;}	//pivot보다 큰 경우 오른쪽에 있는게 맞으므로 -1
+                while(pivot > data[left] && left < end){left++;}		//pivot보다 작은 경우 왼쪽에 있는게 맞으므로 +1
+
+                if(right > left){
+                    temp = data[right];
+                    data[right] = data[left];
+                    data[left] = temp;
                 }
-                k++;    //medium 값인 경우 (pivot1 < data[k] < pivot2)
             }
-            low--;      //swap 하기위한 index 이동
-            great++;    //swap 하기위한 index 이동
-
-            swap(data,start,low);       //pivot1과 교환
-            swap(data,end,great);       //pivot2와 교환
-            //partitioning done
-            dualPivotQuickSort(data,start,low-1);
-            dualPivotQuickSort(data,low+1,great-1);
-            dualPivotQuickSort(data,great+1,end);
+            //right가 가리키는 값이 더 크다면 바꿀 필요가없음
+            if(data[right] < data[start]){
+                temp = data[right];
+                data[right] = data[start];
+                data[start] = temp;
+            }
+            quickSort(data,start,right-1);
+            quickSort(data,right+1,end);
         }
-    }
-    public static void swap(int[] data, int idx1, int idx2){
-        int temp = data[idx1];
-        data[idx1] = data[idx2];
-        data[idx2] = temp;
     }
 }
