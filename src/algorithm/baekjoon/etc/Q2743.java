7af135b5f54c7d9d7bdab04a8d69407c8fae0c88
iamjunsulee <mellow0924@gmail.com>
2018-06-23 11:26:41 +0900

    [BAEKJOON_2743]단어 길이 재기 풀이완료

---
+++ src/algorithm/baekjoon/etc/Q2743.java
@@ -0,0 +1,15 @@
+package algorithm.baekjoon.etc;
+
+import java.io.IOException;
+import java.util.Scanner;
+
+/**
+ * 단어 길이 재기
+ */
+public class Q2743 {
+    public static void main(String[] args) throws IOException {
+        Scanner sc = new Scanner(System.in);
+        String inputs = sc.nextLine();
+        System.out.print(inputs.length());  //문자열 길이 출력하면 끝.
+    }
+}
