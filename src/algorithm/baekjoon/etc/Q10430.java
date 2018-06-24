4c39d1779686cb641690b6c217876e537b8dcf64
iamjunsulee <mellow0924@gmail.com>
2018-06-24 15:37:23 +0900

    [BAEKJOON_10430]나머지 풀이완료

---
+++ src/algorithm/baekjoon/etc/Q10430.java
@@ -0,0 +1,20 @@
+package algorithm.baekjoon.etc;
+
+import java.util.Scanner;
+
+/**
+ * 나머지
+ */
+public class Q10430 {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        int c = sc.nextInt();
+
+        System.out.println((a+b)%c);
+        System.out.println((a%c+b%c)%c);
+        System.out.println((a*b)%c);
+        System.out.println((a%c*b%c)%c);
+    }
+}
