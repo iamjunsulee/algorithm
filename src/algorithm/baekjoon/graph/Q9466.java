34f52bb4b356e5e0e64215f78a844f97594bc217
iamjunsulee <mellow0924@gmail.com>
2018-09-05 17:57:48 +0900

    [BAEKJOON_9466]다른 풀이

--- src/algorithm/baekjoon/graph/Q9466.java
+++ src/algorithm/baekjoon/graph/Q9466.java
@@ -5,9 +5,10 @@ import java.util.Scanner;
 
 public class Q9466 {
 	static boolean[] visited;	//방문 여부
-	static boolean[] finished;	//사이클 종료 여부
-	static int[] adArr;
-	static int cnt;				//사이클 원소 합
+	static int[] visitCount;		//몇번쨰 방문인지
+	static int[] startV;				//시작 정점
+	static int[] adArr;	
+
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		int testCase = sc.nextInt();	//테스트케이스 개수
@@ -16,44 +17,42 @@ public class Q9466 {
 			int num = sc.nextInt();		//학생 수
 			
 			visited = new boolean[num+1];
-			finished = new boolean[num+1];
 			adArr = new int[num+1];
+			visitCount = new int[num+1];
+			startV = new int[num+1];
+			
 			//배열 초기화
 			Arrays.fill(visited, false);
-			Arrays.fill(finished, false);
+			Arrays.fill(visitCount, 0);
+			Arrays.fill(startV, 0);
 			Arrays.fill(adArr, 0);
-			cnt = 0;
 			
 			for(int i = 1;i <= num;i++) {
 				adArr[i] = sc.nextInt();	//학생들 번호 저장
 			}
-			
+			int total = 0;
 			for(int i = 1;i <= num;i++) {
 				if(!visited[i]) {
-					dfs(i);
+					//dfs(i);
+					total += dfs(i,i,1);
 				}
 			}
-			System.out.println(num-cnt);
+			System.out.println(num - total);
 			testCase--;
 		}
 	}
-	
-	public static void dfs(int idx) {
-		visited[idx] = true;	//방문 기록
-		int next = adArr[idx];
-		//System.out.println(idx+" 방문");
-		//System.out.println("다음 방문할 노드 : "+next);
-		if(!visited[next]) {
-			dfs(next);
-		}else {//방문한 경우
-			if(!finished[next]) {
-				for(int i = next;i != idx;i = adArr[i]) {	//사이클 내에 현재 idx가 아닌 다른 점 개수 카운트
-					cnt++;
-					//System.out.println("next : "+next+",i : "+i+",cnt = "+cnt);
-				}
-				cnt++;	//자기 자신 카운트
+	public static int dfs(int start, int current, int cnt) {
+		if(visited[current]) {//방문한 경우
+			System.out.println("startV : "+startV[current]+", start : "+start);
+			if(startV[current] != start) {//정점 시작점이 다를 경우, 사이클 아님
+				return 0;
 			}
+			return cnt-visitCount[current];	//사이클 정점 수
 		}
-		finished[idx] = true;		//사이클 종료
+		visitCount[current] = cnt;	//몇번째 방문인지
+		startV[current] = start;		//시작 정점
+		visited[current] = true;		//방문 기록
+
+		return dfs(start, adArr[current],cnt+1);
 	}
 }
